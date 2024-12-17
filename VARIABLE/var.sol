f1f4fd3ada216d31fa0e43ec459674c04b6f9b22
jeanaimeiraguha <jeanaimeiraguha@gmail.com>
2024-12-17 18:15:05 +0200

    state variable

--- VARIABLE/var.sol
+++ VARIABLE/var.sol
@@ -9,3 +9,11 @@ These are the variables declared inside function
 
 are stored permanently on the block chain and their values are persistent across function calls and contract instances
 */
+
+contract MyContract {
+    uint256 public myStateVariable = 10;
+    
+    function getMyValue() external view returns(uint256) {
+        return myStateVariable; // A simple getter.
+      }
+}
